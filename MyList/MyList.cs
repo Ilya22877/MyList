54ad82ab3bd24e0ce2daf0e6386ddf923d3c7fc9
Ilya <menchinskiyia@skbkontur.ru>
2021-03-09 02:14:52 +0500

    MyList

--- MyList/MyList.cs
+++ MyList/MyList.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MyList
 {
     public class MyList<T> : IList<T>
     {
+        private const int DefaultCapacity = 4;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        private T[] Array { get; set; }
+
+        public MyList() : this(DefaultCapacity)
+        {
+        }
+
+        public MyList(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), "Capacity must be more when 0");
+            }
+
+            Array = new T[capacity];
+        }
+
+        public MyList(IEnumerable<T> collection)
+        {
+            Array = collection.ToArray();
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Enumerator(this);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -18,50 +46,152 @@ namespace MyList
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (Count == Array.Length)
+            {
+                IncreaseArraySize();
+            }
+            Array[Count] = item;
+            Count++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Array = new T[Array.Length];
+            Count = 0;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < Count; i++)
+            {
+                if (Array[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            System.Array.Copy(Array, 0, array, arrayIndex, Count);
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            var index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
         }
 
-        public int Count { get; }
-        public bool IsReadOnly { get; }
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            return System.Array.IndexOf(Array, item, 0, Count);
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (index > Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (Count == Array.Length) IncreaseArraySize();
+            System.Array.Copy(Array, index, Array, index + 1, Count - index);
+            Array[index] = item;
+            Count++;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index > Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Count--;
+            if (index < Count)
+            {
+                System.Array.Copy(Array, index + 1, Array, index, Count - index);
+            }
+            Array[Count] = default(T);
         }
 
         public T this[int index]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return Array[index];
+            }
+
+            set
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Array[index] = value;
+            }
+        }
+
+        private void IncreaseArraySize()
+        {
+            var newCapacity = Array.Length == 0 ? 
+                DefaultCapacity : 
+                Array.Length * 2;
+            var newArray = new T[newCapacity];
+            Array.CopyTo(newArray, 0);
+            Array = newArray;
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private readonly MyList<T> _list;
+            private int _index;
+            private T _current;
+
+            internal Enumerator(MyList<T> list)
+            {
+                _list = list;
+                _index = 0;
+                _current = default(T);
+            }
+
+            public bool MoveNext()
+            {
+                if (_index < _list.Count)
+                {
+                    _current = _list[_index];
+                    _index++;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public T Current => _current;
+
+            Object IEnumerator.Current => Current;
+
+            void IEnumerator.Reset()
+            {
+                _index = 0;
+                _current = default(T);
+            }
+
+            public void Dispose()
+            {
+            }
         }
     }
 }
---
+++ MyList/MyListTests.cs
@@ -0,0 +1,133 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MyList
+{
+    [TestFixture]
+    public class MyListTests
+    {
+        private MyList<int> _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new MyList<int>();
+        }
+
+        [Test]
+        public void ShouldReturnZEro_WhenListEmpty()
+        {
+            _sut.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void MustAddOneElement()
+        {
+            var element = 5;
+
+            _sut.Add(element);
+
+            _sut.Count.Should().Be(1);
+            _sut.First().Should().Be(element);
+        }
+
+        [Test]
+        public void ShouldIncreaseSize_WhenLotsElement()
+        {
+            var size = 1000;
+            while (_sut.Count < size)
+            {
+                var value = _sut.Count;
+                _sut.Add(value);
+                _sut.Last().Should().Be(value);
+            }
+
+            _sut.Count.Should().Be(size);
+        }
+
+        [Test]
+        public void ShouldClearList()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+
+            _sut.Clear();
+
+            _sut.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void ShouldFindElement_WhenUseContains()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+
+            _sut.Contains(1).Should().Be(true);
+        }
+
+        [Test]
+        public void ShouldNotFindDefaultElement_WhenUseContains()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+
+            _sut.Contains(0).Should().Be(false);
+        }
+
+        [Test]
+        public void ShouldCopyToArray()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+            var array = new []{1,0,0,0};
+            var expectedArray = new[] { 1, 1, 2, 0 };
+
+            _sut.CopyTo(array, 1);
+
+            array.Should().BeEquivalentTo(expectedArray);
+        }
+
+        [Test]
+        public void ShouldInsertElement()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+            _sut.Add(3);
+            var expectedResult = new[] { 1, 2, 4, 3 };
+
+            _sut.Insert(2, 4);
+
+            _sut.Count.Should().Be(4);
+            _sut.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void ShouldRemoveElement()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+            _sut.Add(3);
+            var expectedResult = new[] { 1, 3 };
+
+            _sut.Remove( 2);
+
+            _sut.Count.Should().Be(2);
+            _sut.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void ShouldRemoveAtElement()
+        {
+            _sut.Add(1);
+            _sut.Add(2);
+            _sut.Add(3);
+            var expectedResult = new[] { 1, 3 };
+
+            _sut.RemoveAt(1);
+
+            _sut.Count.Should().Be(2);
+            _sut.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
